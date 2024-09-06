fe4af5fb6e254fdad6f74802e770bde3929246e9
Ramin Mamedov <mamedov10rm@mail.ru>
2024-09-06 16:16:32 +0400

    Updates

--- src/selectors/userSelectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import {AppState} from "../store/hooks";
-
-export const selectUserState = (state: AppState) => state.user;
-
-export const selectIsAuthenticated = createSelector(
-    selectUserState,
-    (userState) => userState.isAuthenticated
-);
--- src/store/hooks.ts
+++
@@ -1,8 +0,0 @@
-import {useDispatch, useSelector} from "react-redux";
-import {store} from "./store";
-
-
-export type AppState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export const useAppSelector = useSelector.withTypes<AppState>();
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
